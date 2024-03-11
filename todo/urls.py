b9ed5119854765b194b73cace8a7cb6762856ebf
Serhii Haiduchyk <jaegarn141@gmail.com>
2024-03-11 11:37:22 +0200

    feature: added forms for creation and update tasks.

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,31 @@
 from django.urls import path
 
-from todo.views import index
+from todo.views import (
+    index,
+    TaskCreateView,
+    TaskDeleteView,
+    TaskDetailView,
+    TaskListView,
+    TaskUpdateView,
+)
 
 app_name = "todo"
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("tasks/",
+         TaskListView.as_view(),
+         name="task-list"),
+    path("tasks/<int:pk>/,",
+         TaskDetailView.as_view(),
+         name="task-detail"),
+    path("tasks/<int:pk>/delete/",
+         TaskDeleteView.as_view(),
+         name="task-delete"),
+    path("tasks/<int:pk>/update/",
+         TaskUpdateView.as_view(),
+         name="task-update"),
+    path("task_create/",
+         TaskCreateView.as_view(),
+         name="task-create"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,21 +1,52 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+
+from todo.forms import TaskCreationForm
+from todo.models import Task
 
 
-@login_required
+@login_required()
 def index(request: HttpRequest) -> HttpResponse:
-    # user = request.user
-    #
-    # all_tasks = Task.objects.filter(assignees=user)
-    # completed_tasks = all_tasks.filter(is_completed=True)
-    # incompleted_tasks = all_tasks.filter(is_completed=False)
-    #
-    # context = {
-    #     "all_tasks": all_tasks.count(),
-    #     "completed_tasks_count": completed_tasks.count(),
-    #     "incompleted_tasks_count": incompleted_tasks.count(),
-    # }
-    #
-    # return render(request, "todo/index.html", context)
-    pass
+    all_tasks = Task.objects.all()
+    completed_tasks = all_tasks.filter(is_completed=True)
+    incompleted_tasks = all_tasks.filter(is_completed=False)
+
+    context = {
+        "all_tasks_count": all_tasks.count(),
+        "completed_tasks_count": completed_tasks.count(),
+        "incompleted_tasks_count": incompleted_tasks.count(),
+    }
+
+    return render(request, "todo/index.html", context)
+
+
+class TaskCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Task
+    form_class = TaskCreationForm
+    template_name = "create_task.html"
+    success_url = reverse_lazy("todo:task-list")
+
+
+class TaskDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("todo:task-list")
+
+
+class TaskListView(LoginRequiredMixin, generic.ListView):
+    model = Task
+
+
+class TaskDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Task
+
+
+class TaskUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Task
+    form_class = TaskCreationForm
+    template_name = "task_form.html"
+    success_url = reverse_lazy("todo:task-list")
+
